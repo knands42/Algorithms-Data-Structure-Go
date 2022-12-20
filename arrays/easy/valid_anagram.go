4f85d6b0ac05b74b6b7897640409060a7971d028
Caio <caiow.wk@gmail.com>
2022-12-20 00:37:55 -0300

    feat: add valid anagram

---
+++ arrays/easy/valid_anagram.go
@@ -0,0 +1,32 @@
+package arrays
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	dict := make(map[string]int)
+
+	for _, v := range s {
+		dict[string(v)] += 1
+	}
+
+	for _, char := range t {
+		char := string(char)
+		if amount, ok := dict[char]; ok {
+			if amount == 1 {
+				delete(dict, char)
+			} else {
+				dict[char] -= 1
+			}
+		} else {
+			return false
+		}
+	}
+
+	if len(dict) == 0 {
+		return true
+	}
+
+	return false
+}
